1ca7c038aee8ef8a87e33c5b82bd0458bc718a04
Liliana Milano <milanoliliana129@gmail.com>
2022-11-27 03:11:25 -0400

    Create 'provincias' table and run its migration to the DB

--- app/Models/Provincia.php
+++ app/Models/Provincia.php
@@ -2,10 +2,31 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Provincia extends Model
 {
-    use HasFactory;
+    use SoftDeletes;
+    
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'provincias'; 
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['nombre'];
 }
--- database/migrations/2022_11_26_043639_create_provincias_table.php
+++ database/migrations/2022_11_26_043639_create_provincias_table.php
@@ -15,7 +15,9 @@ return new class extends Migration
     {
         Schema::create('provincias', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
