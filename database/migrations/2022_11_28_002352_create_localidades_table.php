eca7eaebffe77e261c3c7c69aa9b3acea59642ad
Liliana Milano <milanoliliana129@gmail.com>
2022-12-08 02:52:16 -0400

    Update 'localidades' table

--- database/migrations/2022_11_28_002352_create_localidades_table.php
+++ database/migrations/2022_11_28_002352_create_localidades_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProvinciasTable extends Migration
+class CreateLocalidadesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,13 @@ class CreateProvinciasTable extends Migration
      */
     public function up()
     {
-        Schema::create('provincias', function (Blueprint $table) {
+        Schema::create('localidades', function (Blueprint $table) {
             $table->id();
             $table->string('nombre');
+            //Clave foranea
+            $table->unsignedBigInteger('provincia_id');
+            // Linea donde el indico a provincia_id con que clave primaria de que tabla se relaciona
+            $table->foreign('provincia_id')->references('id')->on('provincias');
             $table->timestamps();
             $table->softDeletes();
         });
@@ -28,6 +32,6 @@ class CreateProvinciasTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('provincias');
+        Schema::dropIfExists('localidades');
     }
-};
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProvinciaController;
+use App\Http\Controllers\LocalidadController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,27 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/**
+ * Provincia
+ */
+Route::post('insertProvincias', [ProvinciaController::class, 'store']);
+// Listar todas las provincias
+Route::get('getAllProvincias', [ProvinciaController::class, 'index']);
+// Listar un provincia
+Route::get('getProvincia/{id}', [ProvinciaController::class, 'show']);
+
+
+/**
+* Localidad
+*/
+// Listar todos
+Route::get('getAllLocalidades', [LocalidadController::class, 'index']);
+// Listar una localidad
+Route::get('getLocalidad/{id}', [LocalidadController::class, 'show']);
+// Registrar una localidad
+Route::post('postLocalidad', [LocalidadController::class, 'store']);
+// Actualizar una localidad
+Route::put('updateLocalidad', [LocalidadController::class, 'update']);
+// borrar una localidad
+Route::delete('borrarLocalidad/{id}', [LocalidadController::class, 'destroy']);
