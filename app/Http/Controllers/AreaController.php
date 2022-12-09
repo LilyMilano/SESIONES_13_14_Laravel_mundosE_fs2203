476530a32626a7f0c084e5e7398cb7e1ba2d718b
Liliana Milano <milanoliliana129@gmail.com>
2022-12-09 14:04:28 -0400

    Add Controllers: Area, Barrio, Localidad, Persona, Provincia

--- app/Http/Controllers/AreaController.php
+++ app/Http/Controllers/AreaController.php
@@ -14,17 +14,20 @@ class AreaController extends Controller
      */
     public function index()
     {
-        //
-    }
+        //SELECT id, nombre FROM areas;
+        $areas = Area::select('id', 'nombre')->get();
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        $data = $areas->map(function ($area) {
+            return [
+                'area_id' => $area->id,
+                'area_nombre' => $area->nombre,
+            ];
+        });
+
+        return response()->json([
+            'mensaje' => 'Listado de areas disponibles',
+            'data' => $data,
+        ]);
     }
 
     /**
@@ -35,7 +38,21 @@ class AreaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //Validación
+        $request->validate([
+            'nombre' => ['required'],
+            'interno' => ['required', 'integer', 'max:4'],
+        ]);
+
+        $area = Area::create([
+            'nombre' => $request['nombre'],
+            'interno' => $request['interno'],
+        ]);
+
+        return response()->json([
+            'mensaje' => 'Se Agrego Correctamente el area',
+            'data' => $area,
+        ]);
     }
 
     /**
@@ -44,20 +61,20 @@ class AreaController extends Controller
      * @param  \App\Models\Area  $area
      * @return \Illuminate\Http\Response
      */
-    public function show(Area $area)
+    public function show(int $id)
     {
-        //
-    }
+        $data = Area::findOrFail($id);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Area  $area
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Area $area)
-    {
-        //
+        $area = [
+            'area_id' => $data->id,
+            'area_nombre' => $data->nombre,
+            'area_interno' => $data->interno,
+        ];
+
+        return response()->json([
+            'mensaje' => 'Datos del area solicitada',
+            'data' => $area,
+        ]);
     }
 
     /**
@@ -67,9 +84,22 @@ class AreaController extends Controller
      * @param  \App\Models\Area  $area
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Area $area)
+    public function update(Request $request, int $id)
     {
-        //
+        $request->validate([
+            'nombre' => ['required'],
+            'interno' => ['required', 'integer', 'max:4'],
+        ]);
+
+        $area = Area::findOrFail($id);
+        $area->nombre = $request['nombre'];
+        $area->interno = $request['interno'];
+        $area->save();
+
+        return response()->json([
+            'mensaje' => 'Datos del area modificada',
+            'data' => $area,
+        ]);
     }
 
     /**
@@ -78,8 +108,14 @@ class AreaController extends Controller
      * @param  \App\Models\Area  $area
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Area $area)
+    public function destroy(int $id)
     {
-        //
+        $area = Area::findOrFail($id);
+        $area->delete();
+
+        return response()->json([
+            'mensaje' => 'El area se elimino correctamente',
+            'data' => $area,
+        ]);
     }
 }
--- app/Http/Controllers/PersonaController.php
+++ app/Http/Controllers/PersonaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Persona;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class PersonaController extends Controller
 {
@@ -14,17 +15,19 @@ class PersonaController extends Controller
      */
     public function index()
     {
-        //
-    }
+        $personas = Persona::get();
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        $data = $personas->map(function ($persona) {
+            return [
+                'id' => $persona->id,
+                'nombre' => $persona->nombre,
+            ];
+        });
+
+        return response()->json([
+            'mensaje' => 'Listado de personas disponibles',
+            'data' => $data,
+        ]);
     }
 
     /**
@@ -35,7 +38,32 @@ class PersonaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'nombre' => ['required'],
+            'apellido' => ['required'],
+            'correo' => ['required'],
+            'dni' => ['required', 'integer'],
+            'area_id' => ['required'],
+            'barrio_id' => ['required'],
+            'provincia_id' => ['required'],
+        ]);
+
+        $persona = Persona::create([
+            'nombre' => $request['nombre'],
+            'apellido' => $request['apellido'],
+            'correo' => $request['correo'],
+            'dni' => $request['dni'],
+            'legajo' => Str::random(6),
+            'fechaNacimiento' => $request['fechaNacimiento'],
+            'area_id' => $request['area_id'],
+            'barrio_id' => $request['barrio_id'],
+            'provincia_id' => $request['provincia_id'],
+        ]);
+
+        return response()->json([
+            'mensaje' => 'Se Agrego Correctamente la persona',
+            'data' => $persona,
+        ]);
     }
 
     /**
@@ -44,20 +72,25 @@ class PersonaController extends Controller
      * @param  \App\Models\Persona  $persona
      * @return \Illuminate\Http\Response
      */
-    public function show(Persona $persona)
+    public function show(int $id)
     {
-        //
-    }
+        $data = Persona::with('barrio.localidad.provincia', 'area', 'provincia')->findOrFail($id);
+        //SELECT * FROM personas WHERE id = $id;
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Persona  $persona
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Persona $persona)
-    {
-        //
+        $persona = [
+            'id' => $data->id,
+            'nombre' => $data->nombre,
+            'apellido' => $data->apellido,
+            'barrio' => $data->barrio->nombre,
+            'localidad' => $data->barrio->localidad->nombre,
+            'area' => $data->area->nombre,
+            'provincia_origin' => $data->provincia->nombre,
+        ];
+
+        return response()->json([
+            'mensaje' => 'Datos de la persona solicitada',
+            'data' => $persona,
+        ]);
     }
 
     /**
@@ -67,9 +100,34 @@ class PersonaController extends Controller
      * @param  \App\Models\Persona  $persona
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Persona $persona)
+    public function update(Request $request, int $id)
     {
-        //
+        $request->validate([
+            'nombre' => ['required'],
+            'apellido' => ['required'],
+            'correo' => ['required'],
+            'dni' => ['required', 'integer'],
+            'area_id' => ['required'],
+            'barrio_id' => ['required'],
+            'provincia_id' => ['required'],
+        ]);
+
+        $persona = Persona::findOrFail($id);
+        $persona->nombre = $request['nombre'];
+        $persona->apellido = $request['apellido'];
+        $persona->correo = $request['correo'];
+        $persona->dni = $request['dni'];
+        $persona->legajo = $request['legajo'];
+        $persona->fechaNacimiento = $request['fechaNacimiento'];
+        $persona->area_id = $request['area_id'];
+        $persona->barrio_id = $request['barrio_id'];
+        $persona->provincia_id = $request['provincia_id'];
+        $persona->save();
+
+        return response()->json([
+            'mensaje' => 'Datos de la persona modificado',
+            'data' => $persona,
+        ]);
     }
 
     /**
@@ -78,8 +136,14 @@ class PersonaController extends Controller
      * @param  \App\Models\Persona  $persona
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Persona $persona)
+    public function destroy(int $id)
     {
-        //
+        $persona = Persona::findOrFail($id);
+        $persona->delete();
+
+        return response()->json([
+            'mensaje' => 'La persona se elimino correctamente',
+            'data' => $persona,
+        ]);
     }
 }
